c6c06d6ad3bc1258e353fc9b086fd006c7552b3a
Alieu Baldeh <alieu.dev@gmail.com>
2021-08-18 19:33:41 -0400

    theater

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,40 @@
-class Teacher < User
+require 'pry'
+
+class Role < ActiveRecord::Base
+    has_many :auditions
+
+    def actors
+        self.auditions.map do |t|
+            t.actor
+        end
+    end
+
+    def locations
+        self.auditions.map do |t|
+            t.location
+        end 
+    end
+
+    def lead
+        hired_status = self.auditions.find_by(:hired => true)
+        if hired_status
+            hired_status
+        else
+            "no actor has been hired for this role"
+        end
+    end
+
+    def understudy
+        second_hired = self.auditions.where(:hired => true).second
+        if second_hired
+            second_hired
+        else
+            'no actor has been hired for understudy for this role'
+        end
+
+    end
+    binding.pry
+endclass Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
